9f2770525a668f4aa8a586b2dbde7ae3da0fca32
limeng <845542048@qq.com>
2022-08-11 21:01:04 +0800

    lmm完成了学科第一个页面和功能

--- src/api/hmmm/subjects.js
+++ src/api/hmmm/subjects.js
@@ -1,8 +1,13 @@
-/*
- * @Author: zhanglianchang
- * @Date: 2020-08-01 14:39:32
- * @Last Modified by: taoshiwei
- * @Last Modified time: 2019-04-29 14:53:23
- *
- * 学科有关的接口：自己写吧~~  已经被张老师删了 ^_^
- */
+import { createAPI } from '@/utils/request'
+
+// 学科基础详情
+export const subjectList = data => createAPI('/subjects', 'get', data)
+
+// 学科添加
+export const addsubject = data => createAPI('/subjects', 'post', data)
+
+// 学科修改
+export const editsubject = data => createAPI(`/subjects/${data.id}`, 'PUT', data)
+
+// 学科删除
+export const delsubject = data => createAPI(`/subjects/${data.id}`, 'delete', data)
