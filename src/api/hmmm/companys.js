2d308ef86adce6fb685703bd42c7279d75743430
“邵阳阳” <946715221@qq.com>
2022-08-12 22:23:52 +0800

    SYY 写了一小时终于写完了

--- src/api/hmmm/companys.js
+++ src/api/hmmm/companys.js
@@ -7,6 +7,7 @@
  * 企业管理
  */
 
+<<<<<<< Updated upstream
 import createAPI from '@/utils/request'
 
 export const list = data => createAPI('/comapnys', 'post')
@@ -17,3 +18,21 @@ export const remove = data => createAPI(`/comapnys/${data.id}`, 'get')
 export const detail = data =>
   createAPI(`/comapnys/${data.userId}`, 'post', data)
 export const disabled = data => createAPI('/comapnys/state', 'post', data)
+=======
+import { createAPI } from '@/utils/request'
+// 企业管理列表
+// export const list = data => createAPI('/companys', 'get', data)
+export const list = page => createAPI(`/companys?page=${page.page}&pagesize=${page.pagesize}`, 'get')
+// 添加接口
+export const add = data => createAPI('/companys', 'post', data)
+// 更新接口
+export const update = id => createAPI(`/companys/${id}`, 'put')
+// 删除接口
+export const removeData = id => createAPI(`/companys/${id}`, 'delete')
+
+export const detail = data => createAPI(`/companys/${data.userId}`, 'post', data)
+
+export const disabled = data => createAPI('/companys/state', 'post', data)
+
+export const qiye = data => createAPI('/companys/' + data, 'get')
+>>>>>>> Stashed changes
---
+++ src/module-hmmm/components/pagination-page.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="pagination">
+    <!-- 分页组件 -->
+     <el-pagination
+      background
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+      :current-page="page.page"
+      :page-sizes="[1, 2, 3, 4]"
+      :page-size="Number(page.pagesize)"
+      layout="prev, pager, next,sizes,jumper"
+      :total="page.counts">
+    </el-pagination>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  props: ['page'],
+
+  methods: {
+    // 每页多少条改变的时候触发 val是最新值
+    handleSizeChange (val) {
+      // console.log(`每页 ${val} 条`)
+      this.$emit('changeSize', val)
+    },
+    // 当前是多少页 val是最新值
+    handleCurrentChange (val) {
+      // console.log(`当前页: ${val}`)
+      this.$emit('changeCurrent', val)
+      // console.log(this.page)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.pagination{
+    margin-top: 20px;
+}
+</style>
