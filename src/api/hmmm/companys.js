790de9d7c1a492823da8df3ccf70d3f119a3ed5b
“邵阳阳” <946715221@qq.com>
2022-08-12 22:26:26 +0800

    123

--- src/api/hmmm/companys.js
+++ src/api/hmmm/companys.js
@@ -7,18 +7,6 @@
  * 企业管理
  */
 
-<<<<<<< Updated upstream
-import createAPI from '@/utils/request'
-
-export const list = data => createAPI('/comapnys', 'post')
-export const add = data => createAPI('/comapnys', 'post', data)
-export const update = data =>
-  createAPI(`/comapnys/${data.userId}`, 'get', data)
-export const remove = data => createAPI(`/comapnys/${data.id}`, 'get')
-export const detail = data =>
-  createAPI(`/comapnys/${data.userId}`, 'post', data)
-export const disabled = data => createAPI('/comapnys/state', 'post', data)
-=======
 import { createAPI } from '@/utils/request'
 // 企业管理列表
 // export const list = data => createAPI('/companys', 'get', data)
@@ -35,4 +23,3 @@ export const detail = data => createAPI(`/companys/${data.userId}`, 'post', data
 export const disabled = data => createAPI('/companys/state', 'post', data)
 
 export const qiye = data => createAPI('/companys/' + data, 'get')
->>>>>>> Stashed changes
